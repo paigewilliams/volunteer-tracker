5016b8c12c17ba767fd428af747c268149dc1344
Paige Williams <paw145@humboldt.edu>
2018-12-07 09:20:08 -0800

    Add method to return all prjects; passing.

--- lib/project.rb
+++ lib/project.rb
@@ -4,6 +4,7 @@ class Project
 
   def initialize(attributes)
     @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
   end
 
   def save
@@ -13,6 +14,17 @@ class Project
 
   def ==(another_project)
     self.title().==(another_project.title).&(self.id().==(another_project.id()))
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
 
 end
