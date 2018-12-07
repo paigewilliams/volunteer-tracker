1c6a5538a5cfc77154f45a46fb0da728f0744516
Paige Williams <paw145@humboldt.edu>
2018-12-07 09:45:17 -0800

    Create method to save and return all volunteers; passing.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,27 +7,27 @@ class Volunteer
     @id = attributes.fetch(:id)
     @project_id = attributes.fetch(:project_id)
   end
-  #
-  # def save
-  #   result = DB.exec("INSERT INTO projects (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
-  #   @id = result.first().fetch("id").to_i
-  # end
-  #
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
   def ==(another_volunteer)
     self.name().==(another_volunteer.name).&(self.id().==(another_volunteer.id())).&(self.project_id().==(another_volunteer.project_id()))
   end
 
-  # def self.all
-  #   returned_projects = DB.exec("SELECT * FROM projects;")
-  #   projects = []
-  #   returned_projects.each do |project|
-  #     name = project.fetch("name")
-  #     id = project.fetch("id").to_i
-  #     project_id = project.fetch("project_id").to_i
-  #     projects.push(Project.new({:name => name, :project_id => project_id, :id => id,}))
-  #   end
-  #   projects
-  # end
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      project_id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id,}))
+    end
+    volunteers
+  end
   #
   # def self.find(id)
   #   returned_projects_by_id = DB.exec("SELECT * FROM projects WHERE id = #{id};")
