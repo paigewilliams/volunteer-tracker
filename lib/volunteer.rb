d3ee4143364415a2ec36c2d0543bec15da025d11
Paige Williams <paw145@humboldt.edu>
2018-12-07 09:41:30 -0800

    Add method to alter 'equal to' for volunteers.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,16 +7,16 @@ class Volunteer
     @id = attributes.fetch(:id)
     @project_id = attributes.fetch(:project_id)
   end
-  # 
+  #
   # def save
   #   result = DB.exec("INSERT INTO projects (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
   #   @id = result.first().fetch("id").to_i
   # end
   #
-  # def ==(another_project)
-  #   self.name().==(another_project.name).&(self.id().==(another_project.id())&(self.project_id().==(another_project.project_id()))
-  # end
-  #
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name).&(self.id().==(another_volunteer.id())).&(self.project_id().==(another_volunteer.project_id()))
+  end
+
   # def self.all
   #   returned_projects = DB.exec("SELECT * FROM projects;")
   #   projects = []
