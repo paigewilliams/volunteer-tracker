08f24dfd14103101d53d715546df1fd212953bac
Paige Williams <paw145@humboldt.edu>
2018-12-07 10:44:45 -0800

    Create list of projects when form is submitted; passing first integration spec.

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
+require("pry")
 require("./lib/project")
 require("./lib/volunteer")
 require("pg")
 
-DB = PG.connect({:db => "train_system"})
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
 
 get ("/") do
-  
+  @projects = Project.all
+  erb(:index)
+end
+
+post("/add_project") do
+  title = params.fetch("title")
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
   erb(:index)
 end
